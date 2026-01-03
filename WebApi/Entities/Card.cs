d097e3fd3fdf3d639aa149901bddb3496323d1ba
Teo <teodora.cristiana.stoicescu@gmail.com>
2026-01-03 13:45:00 +0100

    changed type

--- WebApi/Entities/Card.cs
+++ WebApi/Entities/Card.cs
@@ -5,5 +5,5 @@ public class Card
     public int Id { get; set; }
     public required string CardName { get; set; }
     public required int ManaCost { get; set; }
-    public required Boolean IsCreature { get; set; }
+    public required bool IsCreature { get; set; }
 }
